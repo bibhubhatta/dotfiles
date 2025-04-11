458c732bfead6641c9c8bec223c65414118315e4
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-04-11 10:25:40 -0400

    Fix redundant source command for Rust environment setup
    Prevents error when cargo is already installed and "$HOME/.cargo/env" does not exist

--- home/.chezmoiscripts/run_onchange_before_install-crates.sh
+++ home/.chezmoiscripts/run_onchange_before_install-crates.sh
@@ -6,11 +6,11 @@ echo "Checking for Rust installation..."
 if ! command -v cargo &> /dev/null; then
     echo "Installing rust..."
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal -y
+    # shellcheck source=/dev/null
+    source "$HOME/.cargo/env"
 else
     echo "Rust is already installed"
 fi
-# shellcheck source=/dev/null
-source "$HOME/.cargo/env"
 
 echo "Installing cargo-binstall..."
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
