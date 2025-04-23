dbdb31386889d6b3303af76bfbe77507f73a0b94
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-04-23 17:30:39 -0400

    Map common utility commands to alternatives

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -42,4 +42,10 @@ if status is-interactive
     abbr --add --position anywhere -- "--help" "--help | bat --language=help --style=plain"
     abbr --add --position anywhere -- "-h" "-h | bat --language=help --style=plain"
 
+    # replacements for common commands
+    abbr --add "cat" "bat"
+    abbr --add "cd" "z"
+    abbr --add "find" "fd"
+    abbr --add "grep" "rg"
+    abbr --add "ls" "eza"
 end
