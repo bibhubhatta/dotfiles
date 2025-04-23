2ce428f1410769e8b8f07b0e9ccdc080915d0377
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-04-23 16:23:03 -0400

    Adds homebrew path configuration

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -11,3 +11,20 @@ if status is-interactive
     abbr --add "gs" "git status"
     abbr --add "gsw" "git switch"
 end
+
+# homebrew
+# https://github.com/orgs/Homebrew/discussions/4412#discussioncomment-8651316
+if test -d /home/linuxbrew/.linuxbrew # Linux
+	set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
+else if test -d /opt/homebrew # MacOS
+	set -gx HOMEBREW_PREFIX "/opt/homebrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
+end
+if test -d "$HOMEBREW_PREFIX"
+	fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+	! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
+	! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
+end
