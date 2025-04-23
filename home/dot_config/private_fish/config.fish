3087bf41732e904e08cc83fe24c5ae17501551be
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-04-23 16:57:43 -0400

    Source tools in fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,17 +1,3 @@
-if status is-interactive
-    # git abbreviations
-    abbr --add "g" "git"
-    abbr --add "ga" "git add"
-    abbr --add "gc" "git commit"
-    abbr --add "gcl" "git clone"
-    abbr --add "gcm" "git commit -m"
-    abbr --add "gco" "git checkout"
-    abbr --add "gpl" "git pull"
-    abbr --add "gps" "git push"
-    abbr --add "gs" "git status"
-    abbr --add "gsw" "git switch"
-end
-
 # homebrew
 # https://github.com/orgs/Homebrew/discussions/4412#discussioncomment-8651316
 if test -d /home/linuxbrew/.linuxbrew # Linux
@@ -27,4 +13,25 @@ if test -d "$HOMEBREW_PREFIX"
 	fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
 	! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
 	! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
+end
+
+
+if status is-interactive
+    # source tools
+    zoxide init fish | source
+    atuin init fish --disable-up-arrow | source
+    ## fzf.fish plugin is installed, so no need to source it
+    # fzf --fish | source
+
+    # git abbreviations
+    abbr --add "g" "git"
+    abbr --add "ga" "git add"
+    abbr --add "gc" "git commit"
+    abbr --add "gcl" "git clone"
+    abbr --add "gcm" "git commit -m"
+    abbr --add "gco" "git checkout"
+    abbr --add "gpl" "git pull"
+    abbr --add "gps" "git push"
+    abbr --add "gs" "git status"
+    abbr --add "gsw" "git switch"
 end
