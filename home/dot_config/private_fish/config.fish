068deca2900f1ca9f7d1b4a5daf518dd00fdb5b5
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-04-23 16:08:53 -0400

    Adds git abbreviations

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # git abbreviations
+    abbr --add "g" "git"
+    abbr --add "ga" "git add"
+    abbr --add "gc" "git commit"
+    abbr --add "gcl" "git clone"
+    abbr --add "gcm" "git commit -m"
+    abbr --add "gco" "git checkout"
+    abbr --add "gpl" "git pull"
+    abbr --add "gps" "git push"
+    abbr --add "gs" "git status"
+    abbr --add "gsw" "git switch"
 end
