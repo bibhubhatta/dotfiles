db31f334de2eebf05795440d66e09f84b24eee77
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-04-23 20:27:01 -0400

    fish: reindent the config with spaces

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -16,20 +16,24 @@ end
 # homebrew
 # https://github.com/orgs/Homebrew/discussions/4412#discussioncomment-8651316
 if test -d /home/linuxbrew/.linuxbrew # Linux
-	set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
-	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
-	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
+    set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+    set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+    set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
 else if test -d /opt/homebrew # MacOS
-	set -gx HOMEBREW_PREFIX "/opt/homebrew"
-	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
-	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
+    set -gx HOMEBREW_PREFIX /opt/homebrew
+    set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+    set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
 end
 if test -d "$HOMEBREW_PREFIX"
-	fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
-	! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
-	! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
-end
+    fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin"
 
+    ! set -q MANPATH; and set MANPATH ''
+    set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH
+
+    ! set -q INFOPATH; and set INFOPATH ''
+    set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH
+
+end
 
 if status is-interactive
     # source tools
@@ -40,35 +44,35 @@ if status is-interactive
     # fzf --fish | source
 
     # git abbreviations
-    abbr --add "g" "git"
-    abbr --add "ga" "git add"
-    abbr --add "gc" "git commit"
-    abbr --add "gcl" "git clone"
-    abbr --add --set-cursor "gcm" "git commit -m \"%\"" 
-    abbr --add "gco" "git checkout"
-    abbr --add "gpl" "git pull"
-    abbr --add "gps" "git push"
-    abbr --add "gs" "git status"
-    abbr --add "gsw" "git switch"
+    abbr --add g git
+    abbr --add ga "git add"
+    abbr --add gc "git commit"
+    abbr --add gcl "git clone"
+    abbr --add --set-cursor gcm "git commit -m \"%\""
+    abbr --add gco "git checkout"
+    abbr --add gpl "git pull"
+    abbr --add gps "git push"
+    abbr --add gs "git status"
+    abbr --add gsw "git switch"
 
     # eza
-    abbr --add "e" "eza"
+    abbr --add e eza
 
     # format help with bat
-    abbr --add --position anywhere -- "--help" "--help | bat --language=help --style=plain"
-    abbr --add --position anywhere -- "-h" "-h | bat --language=help --style=plain"
+    abbr --add --position anywhere -- --help "--help | bat --language=help --style=plain"
+    abbr --add --position anywhere -- -h "-h | bat --language=help --style=plain"
 
     # replacements for common commands
-    abbr --add "cat" "bat"
-    abbr --add "cd" "z"
-    abbr --add "find" "fd"
-    abbr --add "grep" "rg"
-    abbr --add "ls" "eza"
+    abbr --add cat bat
+    abbr --add cd z
+    abbr --add find fd
+    abbr --add grep rg
+    abbr --add ls eza
 
     # macOS specific abbreviations
-    if test (uname) = "Darwin"
-        abbr --add "o" "open"
-        abbr --add "pbc" "pbcopy"
+    if test (uname) = Darwin
+        abbr --add o open
+        abbr --add pbc pbcopy
     end
 
     # helper functions
